f87a7f661fc2903b400835ec232c36f362f31f5f
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-03-06 18:16:02 +0800

    Migrate lmgtfy to meme subcommands

---
+++ subcommands/meme.ts
@@ -0,0 +1,88 @@
+import { defineSlashSubcommand, defineSubcommand } from 'chooksie'
+import { MessageEmbed } from 'discord.js'
+
+const lmgtfy = defineSubcommand({
+  name: 'lmgtfy',
+  description: 'Post a LMGTFY link.',
+  type: 'SUB_COMMAND',
+  async execute({ interaction }) {
+    const query = interaction.options.getString('query', true)
+    const site = interaction.options.getString('site')
+    const type = interaction.options.getString('type')
+
+    const searchParams = new URLSearchParams({
+      q: query,
+      s: site ?? undefined!,
+      t: type ?? undefined!,
+    })
+
+    const embed = new MessageEmbed()
+      .setTitle(query)
+      .setURL(`https://lmgtfy.app/${searchParams}`)
+      .setDescription('Here you go!')
+
+    await interaction.reply({ embeds: [embed] })
+  },
+  options: [
+    {
+      name: 'query',
+      description: 'The query to LMGTFY.',
+      type: 'STRING',
+      required: true,
+    },
+    {
+      name: 'site',
+      description: 'Use a different search engine.',
+      type: 'STRING',
+      choices: [
+        {
+          name: 'Google',
+          value: 'g',
+        },
+        {
+          name: 'Yahoo',
+          value: 'y',
+        },
+        {
+          name: 'Bing',
+          value: 'b',
+        },
+        {
+          name: 'Ask',
+          value: 'k',
+        },
+        {
+          name: 'AOL',
+          value: 'a',
+        },
+        {
+          name: 'DuckDuckGo',
+          value: 'd',
+        },
+      ],
+    },
+    {
+      name: 'type',
+      description: 'Search type.',
+      type: 'STRING',
+      choices: [
+        {
+          name: 'Web',
+          value: 'w',
+        },
+        {
+          name: 'Images',
+          value: 'i',
+        },
+      ],
+    },
+  ],
+})
+
+export default defineSlashSubcommand({
+  name: 'meme',
+  description: 'Bunch of memes.',
+  options: [
+    lmgtfy,
+  ],
+})
